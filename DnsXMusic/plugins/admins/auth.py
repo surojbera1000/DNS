d3a1ffc68f0f6c23a54acaee109f1e7c7279512f
surojbera1000 <sandipberaind@gmail.com>
2024-12-09 12:57:29 +0530

    Update auth.py

--- DnsXMusic/plugins/admins/auth.py
+++ DnsXMusic/plugins/admins/auth.py
@@ -151,7 +151,7 @@ async def authusers(client, message: Message, _):
         await message.reply_text(text)
 
 
-__MODULE__ = "Auᴛʜ"
+__MODULE__ = "๏ ᴀᴜᴛʜ ๏"
 __HELP__ = """
 
 <b>Aᴜᴛʜ Usᴇʀs ᴄᴀɴ ᴜsᴇ ᴀᴅᴍɪɴ ᴄᴏᴍᴍᴀɴᴅs ᴡɪᴛʜᴏᴜᴛ ᴀᴅᴍɪɴ ʀɪɢʜᴛs ɪɴ ʏᴏᴜʀ ᴄʜᴀᴛ.</b>
