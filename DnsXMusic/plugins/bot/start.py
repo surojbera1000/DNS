05fca68232ab27ac0d1d449065eecd5d8c13522d
surojbera1000 <sandipberaind@gmail.com>
2024-12-15 14:53:32 +0530

    Update start.py

--- DnsXMusic/plugins/bot/start.py
+++ DnsXMusic/plugins/bot/start.py
@@ -1,4 +1,12 @@
-import time
+#
+# Copyright (C) 2024 by MISH0009@Github, < https://github.com/MISH0009 >.
+#
+# This file is part of < https://github.com/MISH0009/DNS > project,
+# and is released under the MIT License.
+# Please see < https://github.com/MISH0009/DNS/blob/master/LICENSE >
+#
+# All rights reserved.
+# time
 import random
 from pyrogram import filters
 from pyrogram.enums import ChatType
